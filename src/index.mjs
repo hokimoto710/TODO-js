6756069959709e86703094fcfe443acede03a4bd
okimo <147290658+hokimoto710@users.noreply.github.com>
2023-10-29 04:35:56 +0000

    Update index.html and index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -8,11 +8,16 @@ const onClickAdd = () => {
   // div生成
   const div = document.createElement("div");
   div.className = "list-row";
-  console.log(div);
 
   // liタグ生成
   const li = document.createElement("li");
   li.innerText("inputText");
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplate-list").appendChild(div);
 };
 
 document
