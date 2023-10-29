81bf88cbd8b75c3cff59a0c529133bb842ecc93b
okimo <147290658+hokimoto710@users.noreply.github.com>
2023-10-29 05:24:56 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -11,10 +11,28 @@ const onClickAdd = () => {
 
   // liタグ生成
   const li = document.createElement("li");
-  li.innerText("inputText");
+  li.innerText = inputText;
+
+  // button(完了)タグ生成
+  const complateButton = document.createElement("button");
+  complateButton.innerText = "完了";
+  complateButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ（div）を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplate-list").removeChild(deleteTarget);
+  });
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(complateButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加
   document.getElementById("incomplate-list").appendChild(div);
