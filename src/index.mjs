2750e987f8e7b931c7294f1ecda241cdb38e70d7
okimo <147290658+hokimoto710@users.noreply.github.com>
2023-10-29 02:44:50 +0000

    Update 3 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,15 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-    const inputText = document.getElementById("add-text").Value;
-    alert(inputText);
-}
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  console.log(div);
+};
 
 document
-    .getElementById("add-button")
-    .addEventListener("click" , () => onClickAdd());
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
